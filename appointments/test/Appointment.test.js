c4f13508ea7a1742332085f2a7960bad808d94d6
d1gitalflow <tiago.m18@gmail.com>
2021-04-04 02:33:23 +0100

    add a test suite

---
+++ appointments/test/Appointment.test.js
@@ -0,0 +1,42 @@
+//Jest already has built in access to global name space and already 'require()ed'.
+//But still need to add the ReactDOM and JSX
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Appointment } from '../Appointment.js'
+
+
+//describes a 'test suite', which is a set of tests with a given name
+//1st arg is the name of the unit of the test.
+describe('Appointment', () => {
+    let container;
+    let customer;
+    
+    //code in this block is executed before each test
+    beforeEach(()=>{
+        //create <div></div>
+        container = document.createElement('div')
+    })
+    
+    //it defines a single/individual test
+    it('renders the customer first name', () => {
+        //create customer obj literal
+        customer = { firstName: 'Ashley' };
+        //final render ReactDOM.render(reactElement,container)
+        ReactDOM.render(<Appointment customer={customer} />, container);
+
+
+        //in each invdidual/single test there can be 
+        //as many as expectations we want
+        expect(container.textContent).toMatch('Ashley');
+    })
+
+    //another single/individual test
+    it('renders another customer first name', () => {
+        customer = { firstName: 'Jordan' };
+        ReactDOM.render(<Appointment customer={customer} />, container);
+        //access <div> textContent ->
+        //represents the text content of the node and its descendants.
+        expect(container.textContent).toMatch('Jordan');
+    });
+})
