91a826a4ad84d7cd98ce9bdd0101f59b074b27ca
d1gitalflow <tiago.m18@gmail.com>
2021-04-12 17:55:50 +0100

    add table row

--- appointments/src/index.js
+++ appointments/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppointmentsDayView } from '../Appointment.js';
+import { AppointmentsDayView } from './AppointmentDayView.js';
 import { sampleAppointments } from './sampleData.js';
 ReactDOM.render(
     <AppointmentsDayView appointments={sampleAppointments} />,
