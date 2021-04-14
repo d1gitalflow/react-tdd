1f8942b720392719ef5b8c08b98568acad431426
d1gitalflow <tiago.m18@gmail.com>
2021-04-14 17:57:59 +0100

    extracting an expectation group function

--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -4,30 +4,34 @@ import { CustomerForm } from '../src/CustomerForm';
 
 
 //a test suite
-describe('CustomerForm',()=>{
-    let render; 
+describe('CustomerForm', () => {
+    let render;
     let container; //div container
 
     //Each test gets its own container,
     //independent of the other tests.
 
     //'render' obj is a function with one parameter waiting to get passed one argument
-    beforeEach(()=>{
-        ({render,container} = createContainer())
+    beforeEach(() => {
+        ({ render, container } = createContainer())
     });
 
-    it('renders a form',()=>{
+    it('renders a form', () => {
         render(<CustomerForm />);
         expect(container.querySelector('form[id="customer"]')).not.toBeNull();
     })
 
-    it('renders the first name field as a text box',() => {
+    const expectToBeInputFieldOfTypeText = formElement => {
+        expect(formElement).not.toBeNull();
+        expect(formElement.tagName).toEqual('INPUT');
+        expect(formElement.type).toEqual('text');
+    };
+
+    it('renders the first name field as a text box', () => {
         render(<CustomerForm />);
         //form('customer') = <form id="customer">
         //For there to be a form element with the name firstName
         const field = form('customer').elements.firstName; //firstName = name attr 
-        expect(field).not.toBeNull();
-        expect(field.tagName).toEqual('INPUT'); //tagName always needs to be upper size
-        expect(field.type).toEqual('text');
+        expectToBeInputFieldOfTypeText(field);
     })
 });
