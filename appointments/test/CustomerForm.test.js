2f067bcaf4afdf4f1135104c98e8563df18d441b
d1gitalflow <tiago.m18@gmail.com>
2021-04-15 04:56:29 +0100

    Extracting out a field-finder function

--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -18,29 +18,31 @@ describe('CustomerForm', () => {
 
     //selector <form id=""
     const form = id => container.querySelector(`form[id="${id}"]`);
+    //access name="storesValue"
+    const firstNameField = () => form('customer').elements.firstName;
 
-    it('renders a form', () => {
-        render(<CustomerForm />);
-        expect(form('customer')).not.toBeNull();
-    })
-
+    //checks form element
     const expectToBeInputFieldOfTypeText = formElement => {
         expect(formElement).not.toBeNull();
         expect(formElement.tagName).toEqual('INPUT');
         expect(formElement.type).toEqual('text');
     };
 
+    it('renders a form', () => {
+        render(<CustomerForm />);
+        expect(form('customer')).not.toBeNull();
+    })
+
     it('renders the first name field as a text box', () => {
         render(<CustomerForm />);
         //to access form controls contained in <form> such as: button, input , etc
         //nodeList = HTMLFormElement.elements returns HTMLFormControlsCollection
-        const field = form('customer').elements.firstName; //firstName is the name="firstName" which is a property, name="" attr stores the value=""
-        expectToBeInputFieldOfTypeText(field);
+        //const field = form('customer').elements.firstName; //firstName is the name="firstName" which is a property, name="" attr stores the value=""
+        expectToBeInputFieldOfTypeText(firstNameField());
     })
 
     it('includes the existing value for the first name',()=>{
         render(<CustomerForm firstName={"Ashley"}/>);
-        const field = form('customer').elements.firstName;
-        expect(field.value).toEqual('Ashley');
+        expect(firstNameField().value).toEqual('Ashley');
     })
 });
